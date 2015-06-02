c850f435e448d1eeb7a930690835e49f499703c2
Kelsey Giusti <kelsey.giusti@gmail.com>
2015-06-02 11:30:52 -0700

    Set Up Hosting

--- config.req.php
+++ config.req.php
@@ -1,6 +1,11 @@
 <?php
 
 	//Database Connectivity config
+/*	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "aplus";*/
+
 	$servername = "localhost";
 	$username = "adminjVRKQJu";
 	$password = "ry18QgZwJZNx";
