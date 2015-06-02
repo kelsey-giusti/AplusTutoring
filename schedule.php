54d3b0555bde6d918acedd5cfc5bdb190022ecba
widkvn <kevwid89@gmail.com>
2015-06-01 18:09:17 -0700

    add include schedule
    add include

--- schedule.php
+++ schedule.php
@@ -123,10 +123,8 @@
 			if (!isset($_SESSION['Name'])) {
 				$login = "show";
 			} else if ($cell === "open"){
-				$servername = "localhost";
-				$username = "root";
-				$password = "";
-				$dbname = "aplus";
+				//include variables from config.req.php for database connectivity
+				include_once 'config.req.php';
 
 				// Create connection
 				$conn = new mysqli($servername, $username, $password, $dbname);
